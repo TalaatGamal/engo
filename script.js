08a64d8571376f110fe9320f836e5d16d678edc0
V-User1 <talaatgamal890@gmail.com>
2024-08-06 21:49:54 +0300

    first commit

--- script.js
+++ script.js
@@ -16,23 +16,23 @@
 
 
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-    const protectedLinks = document.querySelectorAll('.vds');
-    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
+  // document.addEventListener('DOMContentLoaded', (event) => {
+  //   const protectedLinks = document.querySelectorAll('.vds');
+  //   const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
 
-    protectedLinks.forEach(link => {
-      link.addEventListener('click', (event) => {
-        event.preventDefault(); // لمنع التحويل التلقائي للصفحة
-        const userPassword = prompt('Please enter the password:');
+  //   protectedLinks.forEach(link => {
+  //     link.addEventListener('click', (event) => {
+  //       event.preventDefault(); // لمنع التحويل التلقائي للصفحة
+  //       const userPassword = prompt('Please enter the password:');
         
-        if (userPassword === correctPassword) {
-          window.location.href = link.href; // التحويل للصفحة المطلوبة
-        } else {
-          alert('Incorrect password!'); // عرض رسالة خطأ
-        }
-      });
-    });
-  });
+  //       if (userPassword === correctPassword) {
+  //         window.location.href = link.href; // التحويل للصفحة المطلوبة
+  //       } else {
+  //         alert('Incorrect password!'); // عرض رسالة خطأ
+  //       }
+  //     });
+  //   });
+  // });
 
 
                                                // Main Site
