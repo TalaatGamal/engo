47290c8be80ea1d2840c818be16ba22a6fb4d739
V-User1 <talaatgamal890@gmail.com>
2024-08-06 21:18:43 +0300

    first commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 
 
 
-
+                                            // All Videos
 
 
 
@@ -32,4 +32,34 @@
         }
       });
     });
+  });
+
+
+                                               // Main Site
+
+
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
+
+    const form = document.getElementById('form');
+    const passwordInput = document.getElementById('password');
+    const content = document.getElementById('content');
+    const passwordForm = document.getElementById('password-form');
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const userPassword = passwordInput.value;
+
+      if (userPassword === correctPassword) {
+        passwordForm.style.display = 'none';
+        content.style.display = 'block';
+        document.body.style.display = 'block'; // عرض محتوى الصفحة
+      } else {
+        alert('Incorrect password!');
+        window.location.href = 'https://www.google.com'; // تحويل إلى Google
+      }
+    });
+
+    document.body.style.display = 'block'; // عرض نموذج كلمة المرور
   });
