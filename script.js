48e0e5c141cf8ded981fe4d58442d281986cdd04
V-User1 <talaatgamal890@gmail.com>
2024-08-08 22:42:04 +0300

    first commit

--- script.js
+++ script.js
@@ -36,7 +36,50 @@
 
 
 
-    const images = [
+//     const images = [
+//     'url("images/sexy1.jpg")',
+//     'url("images/sexy2.jpeg")',
+//     'url("images/sexy3.jpeg")',
+//     'url("images/sexy4.jpeg")',
+//     'url("images/sexy5.jpeg")',
+//     'url("images/sexy6.jpeg")',
+//     'url("images/sexy7.jpeg")',
+//     'url("images/sexy8.jpg")',
+//     'url("images/sexy9.jpg")',
+//     'url("images/sexy10.jpg")',
+//     'url("images/sexy11.jpg")',
+//     'url("images/sexy12.jpg")',
+//     'url("images/sexy13.jpg")',
+//     'url("images/sexy14.jpg")',
+//     'url("images/sexy15.jpg")'
+// ]
+
+// let currentIndex = 0;
+// const backgroundDiv = document.querySelector('body');
+
+// function changeBackground() {
+//   currentIndex = (currentIndex + 1) % images.length;
+//   backgroundDiv.style.backgroundImage = images[currentIndex];
+// }
+
+// // تغيير الخلفية كل 5 ثواني
+// setInterval(changeBackground, 2000);
+
+// // تعيين الخلفية الأولية
+// backgroundDiv.style.backgroundImage = images[currentIndex];
+
+
+
+
+
+
+
+
+
+
+
+
+const images1 = [
     'url("images/sexy1.jpg")',
     'url("images/sexy2.jpeg")',
     'url("images/sexy3.jpeg")',
@@ -52,23 +95,39 @@
     'url("images/sexy13.jpg")',
     'url("images/sexy14.jpg")',
     'url("images/sexy15.jpg")'
-]
+];
 
-let currentIndex = 0;
-const backgroundDiv = document.querySelector('body');
+const images2 = [
+    'url("images/sexy1.jpg")',
+    'url("images/sexy2.jpeg")',
+    'url("images/sexy3.jpeg")',
+    'url("images/sexy4.jpeg")',
+    'url("images/sexy5.jpeg")',
+    'url("images/sexy6.jpeg")',
+    'url("images/sexy7.jpeg")',
+    'url("images/sexy8.jpg")',
+    'url("images/sexy9.jpg")',
+    'url("images/sexy10.jpg")',
+    'url("images/sexy11.jpg")',
+    'url("images/sexy12.jpg")',
+    'url("images/sexy13.jpg")',
+    'url("images/sexy14.jpg")',
+    'url("images/sexy15.jpg")'
+];
 
-function changeBackground() {
-  currentIndex = (currentIndex + 1) % images.length;
-  backgroundDiv.style.backgroundImage = images[currentIndex];
+let currentIndex1 = 0;
+let currentIndex2 = 0;
+
+function changeBackground(div, images, index) {
+  index = (index + 1) % images.length;
+  div.style.backgroundImage = images[index];
+  return index;
 }
 
-// تغيير الخلفية كل 5 ثواني
-setInterval(changeBackground, 2000);
-
-// تعيين الخلفية الأولية
-backgroundDiv.style.backgroundImage = images[currentIndex];
-
-
+setInterval(() => {
+  currentIndex1 = changeBackground(document.getElementById('body'), images1, currentIndex1);
+  currentIndex2 = changeBackground(document.getElementById('password-form'), images2, currentIndex2);
+}, 2000);
 
 
 
