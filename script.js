aad0d12a168584cff654fa73e68ba83984da04d9
V-User1 <talaatgamal890@gmail.com>
2024-08-09 19:00:06 +0300

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 
   document.addEventListener('DOMContentLoaded', (event) => {
-    const correctPassword = 'sex'; // حدد كلمة السر الصحيحة هنا
+    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
 
     const form = document.getElementById('form');
     const passwordInput = document.getElementById('password');
