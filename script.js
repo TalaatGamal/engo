a440711209b52d4271f53c554c28eb3bb772d04b
V-User1 <talaatgamal890@gmail.com>
2024-08-09 14:47:51 +0300

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 
   document.addEventListener('DOMContentLoaded', (event) => {
-    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
+    const correctPassword = '2486'; // حدد كلمة السر الصحيحة هنا
 
     const form = document.getElementById('form');
     const passwordInput = document.getElementById('password');
@@ -32,52 +32,133 @@
 
 
 
+// // background
+
+
+
+// const images1 = [
+//     'url("images/sexy-1.jpg")',
+//     'url("images/sexy2.jpeg")',
+//     'url("images/sexy3.jpeg")',
+//     'url("images/sexy4.jpeg")',
+//     'url("images/sexy5.jpeg")',
+//     'url("images/sexy6.jpeg")',
+//     'url("images/sexy7.jpeg")',
+//     'url("images/sexy8.jpg")',
+//     'url("images/sexy9.jpg")',
+//     'url("images/sexy10.jpg")',
+//     'url("images/sexy11.jpg")',
+//     'url("images/sexy12.jpg")',
+//     'url("images/sexy13.jpg")',
+//     'url("images/sexy14.jpg")',
+//     'url("images/sexy15.jpg")'
+// ];
+
+// const images2 = [
+//     'url("images/sexy-1.jpg")',
+//     'url("images/sexy2.jpeg")',
+//     'url("images/sexy3.jpeg")',
+//     'url("images/sexy4.jpeg")',
+//     'url("images/sexy5.jpeg")',
+//     'url("images/sexy6.jpeg")',
+//     'url("images/sexy7.jpeg")',
+//     'url("images/sexy8.jpg")',
+//     'url("images/sexy9.jpg")',
+//     'url("images/sexy10.jpg")',
+//     'url("images/sexy11.jpg")',
+//     'url("images/sexy12.jpg")',
+//     'url("images/sexy13.jpg")',
+//     'url("images/sexy14.jpg")',
+//     'url("images/sexy15.jpg")'
+// ];
+
+// let currentIndex1 = 0;
+// let currentIndex2 = 0;
+
+// function changeBackground(div, images, index) {
+//   index = (index + 1) % images.length;
+//   div.style.backgroundImage = images[index];
+//   return index;
+// }
+
+// setInterval(() => {
+//   currentIndex1 = changeBackground(document.getElementById('body'), images1, currentIndex1);
+//   currentIndex2 = changeBackground(document.getElementById('password-form'), images2, currentIndex2);
+// }, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // background
 
-
-
 const images1 = [
-    'url("images/sexy-1.jpg")',
-    'url("images/sexy2.jpeg")',
-    'url("images/sexy3.jpeg")',
-    'url("images/sexy4.jpeg")',
-    'url("images/sexy5.jpeg")',
-    'url("images/sexy6.jpeg")',
-    'url("images/sexy7.jpeg")',
-    'url("images/sexy8.jpg")',
-    'url("images/sexy9.jpg")',
-    'url("images/sexy10.jpg")',
-    'url("images/sexy11.jpg")',
-    'url("images/sexy12.jpg")',
-    'url("images/sexy13.jpg")',
-    'url("images/sexy14.jpg")',
-    'url("images/sexy15.jpg")'
+  'images/sexy-1.jpg',
+  'images/sexy2.jpeg',
+  'images/sexy3.jpeg',
+  'images/sexy4.jpeg',
+  'images/sexy5.jpeg',
+  'images/sexy6.jpeg',
+  'images/sexy7.jpeg',
+  'images/sexy8.jpg',
+  'images/sexy9.jpg',
+  'images/sexy10.jpg',
+  'images/sexy11.jpg',
+  'images/sexy12.jpg',
+  'images/sexy13.jpg',
+  'images/sexy14.jpg',
+  'images/sexy15.jpg'
 ];
 
 const images2 = [
-    'url("images/sexy-1.jpg")',
-    'url("images/sexy2.jpeg")',
-    'url("images/sexy3.jpeg")',
-    'url("images/sexy4.jpeg")',
-    'url("images/sexy5.jpeg")',
-    'url("images/sexy6.jpeg")',
-    'url("images/sexy7.jpeg")',
-    'url("images/sexy8.jpg")',
-    'url("images/sexy9.jpg")',
-    'url("images/sexy10.jpg")',
-    'url("images/sexy11.jpg")',
-    'url("images/sexy12.jpg")',
-    'url("images/sexy13.jpg")',
-    'url("images/sexy14.jpg")',
-    'url("images/sexy15.jpg")'
+  'images/sexy-1.jpg',
+  'images/sexy2.jpeg',
+  'images/sexy3.jpeg',
+  'images/sexy4.jpeg',
+  'images/sexy5.jpeg',
+  'images/sexy6.jpeg',
+  'images/sexy7.jpeg',
+  'images/sexy8.jpg',
+  'images/sexy9.jpg',
+  'images/sexy10.jpg',
+  'images/sexy11.jpg',
+  'images/sexy12.jpg',
+  'images/sexy13.jpg',
+  'images/sexy14.jpg',
+  'images/sexy15.jpg'
 ];
+
+// Function to preload images
+function preloadImages(images) {
+  images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+  });
+}
+
+// Preload all images
+preloadImages(images1);
+preloadImages(images2);
 
 let currentIndex1 = 0;
 let currentIndex2 = 0;
 
 function changeBackground(div, images, index) {
   index = (index + 1) % images.length;
-  div.style.backgroundImage = images[index];
+  div.style.backgroundImage = `url(${images[index]})`;
   return index;
 }
 
@@ -85,8 +166,6 @@ setInterval(() => {
   currentIndex1 = changeBackground(document.getElementById('body'), images1, currentIndex1);
   currentIndex2 = changeBackground(document.getElementById('password-form'), images2, currentIndex2);
 }, 2000);
-
-
 
 
 
