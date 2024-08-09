a9e477f7ed03e84f06a27f0f4e3c38613233f2ec
V-User1 <talaatgamal890@gmail.com>
2024-08-09 15:18:44 +0300

    first commit

--- script.js
+++ script.js
@@ -4,102 +4,30 @@
 
 
 
-  // document.addEventListener('DOMContentLoaded', (event) => {
-  //   const correctPassword = '2486'; // حدد كلمة السر الصحيحة هنا
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
 
-  //   const form = document.getElementById('form');
-  //   const passwordInput = document.getElementById('password');
-  //   const content = document.getElementById('content');
-  //   const passwordForm = document.getElementById('password-form');
+    const form = document.getElementById('form');
+    const passwordInput = document.getElementById('password');
+    const content = document.getElementById('content');
+    const passwordForm = document.getElementById('password-form');
 
-  //   form.addEventListener('submit', (event) => {
-  //     event.preventDefault();
-  //     const userPassword = passwordInput.value;
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const userPassword = passwordInput.value;
 
-  //     if (userPassword === correctPassword) {
-  //       passwordForm.style.display = 'none';
-  //       content.style.display = 'block';
-  //       document.body.style.display = 'block'; // عرض محتوى الصفحة
-  //     } else {
-  //       alert('Incorrect password!');
-  //       window.location.href = 'https://www.google.com'; // تحويل إلى Google
-  //     }
-  //   });
+      if (userPassword === correctPassword) {
+        passwordForm.style.display = 'none';
+        content.style.display = 'block';
+        document.body.style.display = 'block'; // عرض محتوى الصفحة
+      } else {
+        alert('Incorrect password!');
+        window.location.href = 'https://www.google.com'; // تحويل إلى Google
+      }
+    });
 
-  //   document.body.style.display = 'block'; // عرض نموذج كلمة المرور
-  // });
-
-
-
-
-// // background
-
-
-
-// const images1 = [
-//     'url("images/sexy-1.jpg")',
-//     'url("images/sexy2.jpeg")',
-//     'url("images/sexy3.jpeg")',
-//     'url("images/sexy4.jpeg")',
-//     'url("images/sexy5.jpeg")',
-//     'url("images/sexy6.jpeg")',
-//     'url("images/sexy7.jpeg")',
-//     'url("images/sexy8.jpg")',
-//     'url("images/sexy9.jpg")',
-//     'url("images/sexy10.jpg")',
-//     'url("images/sexy11.jpg")',
-//     'url("images/sexy12.jpg")',
-//     'url("images/sexy13.jpg")',
-//     'url("images/sexy14.jpg")',
-//     'url("images/sexy15.jpg")'
-// ];
-
-// const images2 = [
-//     'url("images/sexy-1.jpg")',
-//     'url("images/sexy2.jpeg")',
-//     'url("images/sexy3.jpeg")',
-//     'url("images/sexy4.jpeg")',
-//     'url("images/sexy5.jpeg")',
-//     'url("images/sexy6.jpeg")',
-//     'url("images/sexy7.jpeg")',
-//     'url("images/sexy8.jpg")',
-//     'url("images/sexy9.jpg")',
-//     'url("images/sexy10.jpg")',
-//     'url("images/sexy11.jpg")',
-//     'url("images/sexy12.jpg")',
-//     'url("images/sexy13.jpg")',
-//     'url("images/sexy14.jpg")',
-//     'url("images/sexy15.jpg")'
-// ];
-
-// let currentIndex1 = 0;
-// let currentIndex2 = 0;
-
-// function changeBackground(div, images, index) {
-//   index = (index + 1) % images.length;
-//   div.style.backgroundImage = images[index];
-//   return index;
-// }
-
-// setInterval(() => {
-//   currentIndex1 = changeBackground(document.getElementById('body'), images1, currentIndex1);
-//   currentIndex2 = changeBackground(document.getElementById('password-form'), images2, currentIndex2);
-// }, 2000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    document.body.style.display = 'block'; // عرض نموذج كلمة المرور
+  });
 
 
 
